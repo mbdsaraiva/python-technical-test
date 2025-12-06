b86db7204f9ba37774e0c7f48335381cdd0176ab
mbdsaraiva <matheus.saraiva.083@ufrn.edu.br>
2025-12-06 11:04:15 -0300

    urls.py update

--- academia_dev/urls.py
+++ academia_dev/urls.py
@@ -1,29 +1,13 @@
-"""
-URL configuration for academia_dev project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    # admin do Django
+    # Admin do Django
     path('admin/', admin.site.urls),
     
-    # APIs do DRF
-    path('api/', include('core.urls')),
+    # Páginas HTML e APIs (tudo no core)
+    path('', include('core.urls')),
     
-    # interface navegavel do DRF (para testes)
+    # Interface navegável do DRF (para testes)
     path('api-auth/', include('rest_framework.urls')),
-] 
+]
