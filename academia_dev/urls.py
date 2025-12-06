a41ae7fc6ec78e1219fb7a371019d77dbfe225df
Matheus Saraiva <matheus.saraiva.083@ufrn.edu.br>
2025-12-06 13:57:21 -0300

    changes

--- academia_dev/urls.py
+++ academia_dev/urls.py
@@ -2,12 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    # Admin do Django
     path('admin/', admin.site.urls),
     
-    # Páginas HTML e APIs (tudo no core)
     path('', include('core.urls')),
     
-    # Interface navegável do DRF (para testes)
     path('api-auth/', include('rest_framework.urls')),
 ]
